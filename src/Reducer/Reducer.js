11feca1ec0e55fb33f660c2ebb92a4b3bd59d9f3
Danny <74186971+Danny38-pag@users.noreply.github.com>
2022-08-30 13:40:05 +0000

    redux

---
+++ src/Reducer/Reducer.js
@@ -0,0 +1,10 @@
+let initialState = {users:[
+		{ name: " Naa", contact: " 024443536", location: " Ghana", id: "vsdvsdvc " },
+		{ name: " Wini", contact: " +1 244535645", location: " USA", id: "sesfsdv" },
+	]}
+
+
+const Reducer = (state = initialState) => {
+    return state;
+}
+export default Reducer
