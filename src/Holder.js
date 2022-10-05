9acde5f6663b95d4aae8506359a05bf8c497ef82
Danny <74186971+Danny38-pag@users.noreply.github.com>
2022-10-05 13:18:58 +0000

    read=firebase

--- src/Holder.js
+++ src/Holder.js
@@ -1,43 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {  Container } from 'react-bootstrap';
 import FormCom from './FormCom';
 import Contact from './Contact';
 import "./holder.css";
+import { ADDNEW_USER } from './Action/UserAction';
+import { collection, query, onSnapshot,  orderBy } from "firebase/firestore";
+import { db } from "./firebase/configuer";
+import { useDispatch } from "react-redux";
 
 const Holder = () => {
-    const [usersContact, setUsersContact] = useState([
-		// { name: " Naa", contact: " 024443536", location: " Ghana", id: "vsdvsdvc " },
-		// { name: " Wini", contact: " +1 244535645", location: " USA", id: "sesfsdv" },
-	]);
-   const addContact = (user) => {
-		setUsersContact([...usersContact, user])
-	};
-  const deleteUser = (id) => {
-    setUsersContact(
-      usersContact.filter((usersContact) => {
-        if (usersContact.id !== id) {
-          return usersContact;
-        }
-      })
-    )
-  }
-  const Edituser = (id, newData) => {
-    setUsersContact(
-      usersContact.map((user) => {
-        if (user.id === id) {
-          return newData
-        }
-        return user;
-      })
-    )
-  }
+    const dispatch = useDispatch();
+    useEffect(()=>{
+      const readdata =  async() => {
+      const q = query(collection(db, "mycoontact"));
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const cities = [];
+        querySnapshot.forEach((doc) => {
+            cities.push(doc.data());
+        });
+        dispatch(ADDNEW_USER(cities))
+        console.log(cities);
+});
+    }
+    readdata()
+    },[])
+   
+  // const deleteUser = (id) => {
+  //   setUsersContact(
+  //     usersContact.filter((usersContact) => {
+  //       if (usersContact.id !== id) {
+  //         return usersContact;
+  //       }
+  //     })
+  //   )
+  // }
+  // const Edituser = (id, newData) => {
+  //   setUsersContact(
+  //     usersContact.map((user) => {
+  //       if (user.id === id) {
+  //         return newData
+  //       }
+  //       return user;
+  //     })
+  //   )
+  // }
 
   return (
     <div className='holder'>
         <Container>
-            <FormCom newContact={addContact} />
-            <Contact contactData={usersContact} delete={deleteUser} editUser={Edituser} />
+            <FormCom 
+            // newContact={addContact} 
+            />
+            <Contact 
+            // contactData={usersContact} delete={deleteUser} editUser={Edituser} 
+            />
         </Container>
       
     </div>
--- src/Reducer/Reducer.js
+++ src/Reducer/Reducer.js
@@ -1,14 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
 let initialState = {users:[
-		{ name: " Naa", contact: " 024443536", location: " Ghana", id: uuidv4() },
-		{ name: " Wini", contact: " +1 244535645", location: " USA", id: uuidv4() },
+		// { name: " Naa", contact: " 024443536", location: " Ghana", id: uuidv4() },
+		// { name: " Wini", contact: " +1 244535645", location: " USA", id: uuidv4() },
 	]}
 
 
 const Reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case "ADD_NEW_USER":
-			return {...state,  users : [...state.users, action.payload]}
+			return {...state,  users : action.payload}
 		case "DELETE_USER":
 			const tempUser = state.users.filter((user) => user.id !== action.payload);
 			return { ...state, users: tempUser };	
